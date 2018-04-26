fd3c91fa1fc9e9b5fab8f353277305cc3d6db11f
palandeta <palandeta@utn.edu.ec>
2018-04-25 22:24:37 -0500

    Cambios en cadena de conexión PGSQL
    cadena de conexión para PGSQL

--- database.php
+++ database.php
@@ -1,10 +1,16 @@
 <?php
 class Database 
 {
+	/*
 	private static $dbName = 'productos' ; 
 	private static $dbHost = 'mysql23669-pablo.jl.serv.net.mx' ;
 	private static $dbUsername = 'root';
 	private static $dbUserPassword = 'ATCoch59681';
+	*/
+	private static $dbName = 'sampledb' ; 
+	private static $dbHost = '10.129.68.32' ;
+	private static $dbUsername = 'pablo';
+	private static $dbUserPassword = 'Pablolandeta2018';
 	
 	private static $cont  = null;
 	
@@ -19,7 +25,8 @@ class Database
        {      
         try 
         {
-          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);  
+          //self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);  
+		self::$cont =  new PDO( "pgsql:host=$dbHost;port=5432;dbname=$dbName;user=$dbUsername;password=$dbUserPassword");
         }
         catch(PDOException $e) 
         {
